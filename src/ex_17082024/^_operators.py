0be319a46fd484765872708d124d053a5a535987
manjeetsaini85 <54975401+manjeetsaini85@users.noreply.github.com>
2024-08-17 16:50:49 +0530

    17 August 2024 Home work questions with solutions

---
+++ src/ex_17082024/^_operators.py
@@ -0,0 +1,6 @@
+# What does the ^ operator do in Python, and in what context is it commonly used?
+# it sets the answer to "true" only if one bit is "true"
+# and false if both are set to either "True" or "false"
+input1 = False
+input2 = False
+print(input1 ^ input2)
