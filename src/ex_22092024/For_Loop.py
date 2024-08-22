1bb0506be6e9427659f3f7f8a1a384dcb18ea4c2
Monika9728 <monikasaini704@gmail.com>
2024-08-22 17:37:58 +0530

    20 August 2024 Home work questions with solutions

---
+++ src/ex_22092024/For_Loop.py
@@ -0,0 +1,14 @@
+# range i is variable name
+
+
+# for i in range(1, 10, 1): go to 1 upto 10
+#     print(i)
+# for i in range(1, 5, 2):
+#     print("Python", end=" ")
+
+# for i in range(3, 5):
+#      print(1)
+for x in range(10, 0, -2):
+     print(x)
+# for i in range(0, 11):
+#     print("hello", i)
